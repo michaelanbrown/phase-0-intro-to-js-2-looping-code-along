8e76fd99018734f38bbb67bab61b2c9adf7b1f4d
Michaela Brown <m_brownbrown@yahoo.com>
2022-09-29 11:48:36 -0700

    commit tree 6490f1a6d85122df327879a3bf2e0c4341ad9f05

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(arrayOfStrings, eventName) {
+    let arrayOfMessages = [];
+    for (let i = 0; i < arrayOfStrings.length; i++) {
+      arrayOfMessages.push(`Thank you, ${arrayOfStrings[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return arrayOfMessages;
+  }
+
+  function countDown(n) {
+    let i = 0;
+    while (i <= n) {
+      console.log(n);
+      n--;
+    }
+  }
